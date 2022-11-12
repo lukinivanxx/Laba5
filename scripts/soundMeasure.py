33c3cc4ff3e5229846aa82142d03e3140b08e200
Pchel <fakirian.av@phystech.edu>
2022-11-12 14:05:47 +0300

    Corrected

--- scripts/soundMeasure.py
+++ scripts/soundMeasure.py
@@ -12,3 +12,14 @@ for i in range (0, 5001): # строим 2 массива на 5 тыс знач
     p.append(sf.speed_of_sound(24, 2990, q[i])) #24 град. C , давление насыщенного пара при 24 град., ~ (нигде не было написано: сколько градусов и какая влажность была)
     f.write(str(q[i])+' '+str(p[i])+'\n')
 k=(p[0]-p[5000])/(q[5000]-q[0]) #убеждаемся, что график приближённо линеен, и определяем модуль коэффициента наклона (функция имеет вид: a(x3)=p[0]-k*x3)
+f.close()
+# --- Поиск экспериментально полученных значений скорости звука --- #
+collected = {}
+clear_air = sf.experimental_v('\\data\\1-clear.txt')
+no = sf.experimental_v('\\data\\1-no.txt')
+collected[clear_air]=round((p[0]-clear_air)/k, 3) #расчёт концентраций CO2
+collected[no]=round((p[0]-no)/k, 3) #https://www.kindpng.com/picc/m/134-1346891_transparent-will-smith-png-will-smith-meme-png.png
+f = open(dat_dir, 'a')
+f.write(f'\n{str(clear_air)} {collected[clear_air]}\n{str(no)} {collected[no]}')
+f.close()
+
